4b97956b94551aaaa0ccdbd18053da962802b33e
Frank Strieter <fs@co-it.eu>
2018-03-20 08:03:22 +0100

    removes dist-folder

--- dist/models/enums/note-color.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var NoteColor;
-(function (NoteColor) {
-    NoteColor["YELLOW"] = "yellow";
-    NoteColor["BLUE"] = "blue";
-})(NoteColor = exports.NoteColor || (exports.NoteColor = {}));
-//# sourceMappingURL=note-color.enum.js.map
--- dist/models/services/note.service.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=note.service.js.map

9a065fa06d8aeef11612292d32fa69ae90163f4f
Frank Strieter <fs@co-it.eu>
2018-03-20 08:05:46 +0100

    formats code

--- app.ts
+++ app.ts
@@ -5,31 +5,42 @@ import { pluralize } from "./utils/modifiers/pluralize.template";
 import { reverse } from "./utils/modifiers/reverse.generic";
 
 const note: Note = new Note(3, "spartakiade", "Hello Developer");
-const noteWithTodo: NoteWithTodos = new NoteWithTodos(1, "Typescript ist Toll", "das ist wirklich so", [
-	{
-		title: "Doku lesen",
-		checked: true,
-	},
-]);
+const noteWithTodo: NoteWithTodos = new NoteWithTodos(
+  1,
+  "Typescript ist Toll",
+  "das ist wirklich so",
+  [
+    {
+      title: "Doku lesen",
+      checked: true,
+    },
+  ]
+);
 
-noteWithTodo.addTodo({item: {title: "dependencies installieren", checked: false},position: "first"});
-
-
+noteWithTodo.addTodo({
+  item: { title: "dependencies installieren", checked: false },
+  position: "first",
+});
 
 const notesService: NotesService = new NotesService();
-notesService.notes = [new Note(2, "Developer OpenSpace", "42"), note, noteWithTodo];
+notesService.notes = [
+  new Note(2, "Developer OpenSpace", "42"),
+  note,
+  noteWithTodo,
+];
 
-const archivedNote: Note & IamAnArchivedElement =
-    Object.assign(note, {archiveId: 12});
-    console.log(typeof archivedNote);
+const archivedNote: Note & IamAnArchivedElement = Object.assign(note, {
+  archiveId: 12,
+});
+console.log(typeof archivedNote);
 // const sortArray: (notes: Note[]) => Note[] = (notes: Note[]) =>
 // notes.sort((current, next)=> current.position-next.position);
-
-
 
 // console.log(pluralize`NoteCount: ${notesService.notes.length} Notes`);
 // console.log(pluralize`NoteCount: ${1} Notes`);
 console.log(reverse<Note>(notesService.notes));
 console.log(notesService.listTitles(" | "));
-[notesService.notes[0], notesService.notes[1]] = [notesService.notes[1], notesService.notes[0]];
-
+[notesService.notes[0], notesService.notes[1]] = [
+  notesService.notes[1],
+  notesService.notes[0],
+];
